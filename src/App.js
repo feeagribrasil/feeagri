57cd1c1132aa77f91f3ea7eae44094a44f4154f6
andre-bandeli <andre.bandeli@gmail.com>
2025-04-15 01:15:46 -0300

    analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./paginas/Home";
 import About from "./paginas/About";
@@ -22,6 +23,7 @@ function App() {
 
   return (
     <Routes>
+      <Analytics/>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
       <Route path="/atividades/:slug" element={<Content />} />
